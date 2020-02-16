ce0c0b427b763b380229578c8d803beddce24a62
Jiaxun Yang <jiaxun.yang@flygoat.com>
2020-02-16 15:16:56 +0800

    tmp3

--- src/mips/types.h
+++ src/mips/types.h
@@ -31,11 +31,11 @@
 #include <msa.h>
 #undef pixel
 
-#define u8h_to_u16(v) ((u16x8) vec_mergeh((u8x16) v, vec_splat_u8(0)))
-#define u8l_to_u16(v) ((u16x8) vec_mergel((u8x16) v, vec_splat_u8(0)))
-#define u16h_to_i32(v) ((i32x4) vec_mergeh((u16x8) v, vec_splat_u16(0)))
-#define i16h_to_i32(v) ((i32x4) vec_unpackh((i16x8)v))
-#define u16l_to_i32(v) ((i32x4) vec_mergel((u16x8) v, vec_splat_u16(0)))
-#define i16l_to_i32(v) ((i32x4) vec_unpackl((i16x8)v))
+#define u8h_to_u16(v) ((u16x8) __msa_ilvev_b((v16u8) v, __msa_fill_b(0)))
+#define u8l_to_u16(v) ((u16x8) __msa_ilvod_b((v16u8) v, __msa_fill_b(0)))
+#define u16h_to_i32(v) ((v4i32) __msa_ilvev_h((u16x8) v, __msa_fill_h(0)))
+//#define i16h_to_i32(v) ((v4i32) vec_unpackh((v8i16)v))
+#define u16l_to_i32(v) ((v4i32) __msa_ilvod_h((u16x8) v, __msa_fill_h(0)))
+//#define i16l_to_i32(v) ((v4i32) vec_unpackl((v8i16)v))
 
 #endif /* DAV1D_SRC_PPC_TYPES_H */
